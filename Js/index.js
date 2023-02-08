5daa81aaf8d11e1c65090c7fb14ce3bc266ec3de
Muhammadnuman11 <numanirshad78@gmail.com>
2023-02-08 11:37:55 +0500

    Bidlnn project

---
+++ Js/index.js
@@ -0,0 +1,24 @@
+function myFunction1() {
+    var item1 = document.getElementById("item1");
+    item1.classList.toggle("hide");
+ }
+
+function myFunction2() {
+    var item2 = document.getElementById("item2");
+    item2.classList.toggle("hide");
+ }
+
+function myFunction3() {
+    var item3 = document.getElementById("item3");
+    item3.classList.toggle("hide");
+ }
+
+function myFunction4() {
+    var item4 = document.getElementById("item4");
+    item4.classList.toggle("hide");
+ }
+
+function myFunction5() {
+    var item5 = document.getElementById("item5");
+    item5.classList.toggle("hide");
+ }
